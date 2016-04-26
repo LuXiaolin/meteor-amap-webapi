771d55fce8c1fc74c65a2dc4895bf1dd8e2cb96a
Peng Huijian <peng.huijian@roadshr.com>
2016-04-26 22:58:59 +0800

    Add summary.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-http://lbs.amap.com/api/webservice/summary/
-Web服务API简介
+## [高德地图](http://lbs.amap.com/api/webservice/summary/)
 高德Web服务API向开发者提供HTTP接口，开发者可通过HTTP请求获取各类型的地理数据服务，返回结果支持JSON和XML格式。 
--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'roadshr:amap-webapi',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Amap api for meteor',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/zuizuihao/meteor-amap-webapi',
   // By default, Meteor will default to using README.md for documentation.
