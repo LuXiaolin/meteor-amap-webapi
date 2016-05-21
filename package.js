ffdc1cd002ddfb42c85e2ef5812b4c8ae895e977
Sam Peng <sam.peng@roadshr.com>
2016-05-21 15:39:45 +0800

    Modify package name.

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'roadshr:amap-webapi',
+  name: 'roadshr:meteor-amap-webapi',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'Amap api for meteor',
